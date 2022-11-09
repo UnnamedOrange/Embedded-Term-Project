def6a5ab7d02da92b315a2be953ca72ff72fea0f
UnnamedOrange <lycheng1215@sina.com>
2022-11-09 18:31:43 +0800

    Create empty project

---
+++ embedded-user/main.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file main.cpp
+ * @author UnnamedOrange
+ * @brief 嵌入式软件模块单独运行的入口。
+ *
+ * @copyright Copyright (c) UnnamedOrange. Licensed under the MIT License.
+ * See the LICENSE file in the repository root for full license text.
+ */
+
+int main(int argn, char** argv) {}
