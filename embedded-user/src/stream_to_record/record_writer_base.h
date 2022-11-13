cc206850744ebe77c8122c80c233692d395c97c5
UnnamedOrange <lycheng1215@sina.com>
2022-11-13 12:37:42 +0800

    Create record writer base class

---
+++ embedded-user/src/stream_to_record/record_writer_base.h
@@ -0,0 +1,33 @@
+/**
+ * @file record_writer_base.h
+ * @author UnnamedOrange
+ * @brief 数据写入器基类。
+ *
+ * @copyright Copyright (c) UnnamedOrange. Licensed under the MIT License.
+ * See the LICENSE file in the repository root for full license text.
+ */
+
+#pragma once
+
+#include "record_receive_i.h"
+
+namespace user
+{
+    /**
+     * @brief 数据写入器基类。
+     *
+     * @todo 设计成员变量。
+     * @todo 设计成员函数。
+     */
+    class record_writer_base : virtual public record_receive_i
+    {
+    private:
+        // TODO: 设计成员变量。
+
+    public:
+        virtual ~record_writer_base() = default;
+
+    public:
+        // TODO: 设计成员函数。
+    };
+} // namespace user
