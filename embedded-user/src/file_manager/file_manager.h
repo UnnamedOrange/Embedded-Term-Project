d5bf03a594eed0cbca35716b785f8cacc977aa33
UnnamedOrange <lycheng1215@sina.com>
2022-11-13 12:53:53 +0800

    Create file manager class

---
+++ embedded-user/src/file_manager/file_manager.h
@@ -0,0 +1,31 @@
+/**
+ * @file file_manager.h
+ * @author UnnamedOrange
+ * @brief 数据文件管理器。
+ *
+ * @copyright Copyright (c) UnnamedOrange. Licensed under the MIT License.
+ * See the LICENSE file in the repository root for full license text.
+ */
+
+#pragma once
+
+#include <string>
+
+namespace user
+{
+    /**
+     * @brief 数据文件管理器。
+     *
+     * @todo 设计成员函数。
+     */
+    class file_manager
+    {
+    public:
+        /**
+         * @brief 获取文件列表。文件名之间用换行符分隔，保证结尾是空行。
+         *
+         * @todo 实现获取文件列表。
+         */
+        std::string file_list() const {}
+    };
+} // namespace user
