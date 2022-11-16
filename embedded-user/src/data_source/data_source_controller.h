782c59b52fe3fc1c4194806fcd10057dc9a9950e
UnnamedOrange <lycheng1215@sina.com>
2022-11-16 19:01:39 +0800

    Create config classes

---
+++ embedded-user/src/data_source/data_source_controller.h
@@ -0,0 +1,30 @@
+/**
+ * @file data_source_controller.h
+ * @author UnnamedOrange
+ * @brief 数据源控制器。
+ *
+ * @copyright Copyright (c) UnnamedOrange. Licensed under the MIT License.
+ * See the LICENSE file in the repository root for full license text.
+ */
+
+#pragma once
+
+#include <config/config_changed_i.h>
+
+namespace user
+{
+    /**
+     * @brief 数据源控制器。
+     *
+     * @todo 设计成员变量。
+     * @todo 设计成员函数。
+     */
+    class data_source_controller : public config_changed_i
+    {
+    private:
+        // TODO: 设计成员变量。
+
+    public:
+        // TODO: 设计成员函数。
+    };
+} // namespace user
