b3bc781e89b78335cb9ebbffdce20736a7b45d1b
UnnamedOrange <lycheng1215@sina.com>
2022-11-23 20:48:28 +0800

    Implement source_to_stream_base

--- embedded-user/src/source_to_stream/source_to_stream_base.h
+++ embedded-user/src/source_to_stream/source_to_stream_base.h
@@ -10,6 +10,9 @@
 #pragma once
 
 #include <optional>
+#include <vector>
+
+#include <readerwriterqueue.h>
 
 #include <data_source/data_source_utility.h>
 
@@ -17,13 +20,18 @@ namespace user
 {
     /**
      * @brief 数据流裁剪基类。
-     *
-     * @todo 设计成员变量。
      */
     class source_to_stream_base
     {
+    public:
+        /**
+         * @brief 定义存储回退字节的容器类型。
+         */
+        using unput_bytes_t = std::vector<byte_t>;
+
     private:
-        // TODO: 设计成员变量。
+        moodycamel::ReaderWriterQueue<byte_t> queue; // 来自数据源的字节流。
+        unput_bytes_t unput_bytes;                   // 回退的字节。
 
     public:
         virtual ~source_to_stream_base() = default;
@@ -33,10 +41,40 @@ namespace user
          * @brief 异步发送字节流。
          *
          * @note 子线程调用该函数，将数据保存到缓冲区中，待核心线程处理。
-         *
-         * @todo 实现异步发送字节流。
          */
-        void async_push(const byte_array_t& byte_array) {}
+        void async_push(const byte_array_t& byte_array)
+        {
+            for (const auto& byte : byte_array)
+                queue.enqueue(byte);
+        }
+
+    protected:
+        /**
+         * @brief 核心线程读取一个字节。
+         *
+         * @return std::optional<byte_t>
+         * 如果缓冲区中存在字节，则返回该字节。否则返回空。
+         */
+        std::optional<byte_t> next_byte()
+        {
+            byte_t ret;
+            // 先返回回退的字节。
+            if (!unput_bytes.empty())
+            {
+                ret = unput_bytes.back();
+                unput_bytes.pop_back();
+                return ret;
+            }
+            // 再尝试从队列中取出字节。
+            else if (queue.try_dequeue(ret))
+                return ret;
+            // 下一个字节尚未到来。
+            return std::nullopt;
+        }
+        /**
+         * @brief 核心线程回退一个字节到缓冲区。
+         */
+        void unput(byte_t byte) { unput_bytes.push_back(byte); }
 
     public:
         /**
