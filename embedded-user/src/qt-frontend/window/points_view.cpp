b972f99bf26f2d08fc9c046cab2b0c9327c4257d
UnnamedOrange <lycheng1215@sina.com>
2022-12-01 00:38:24 +0800

    Implement points_view

--- embedded-user/src/qt-frontend/window/points_view.cpp
+++ embedded-user/src/qt-frontend/window/points_view.cpp
@@ -8,7 +8,25 @@
 
 #include "points_view.h"
 
-add_source_to_record::add_source_to_record(QWidget* parent) : QWidget(parent)
+#include <QPainter>
+
+points_view::points_view(
+    std::shared_ptr<user::stream_to_record_base> stream_to_record,
+    QWidget* parent)
+    : QWidget(parent), stream_to_record(stream_to_record)
 {
     ui.setupUi(this);
+}
+
+void points_view::paintEvent(QPaintEvent* event)
+{
+    QPainter painter(this);
+    // 绘制坐标轴。
+    painter.drawLine(0, height() / 2, width(), height() / 2);
+    painter.drawLine(width() / 2, 0, width() / 2, height());
+    // 绘制点。
+    for (size_t i = 0; i < points.size(); i++)
+    {
+        painter.drawPoint(i, points[i]);
+    }
 }
--- embedded-user/src/qt-frontend/window/points_view.h
+++ embedded-user/src/qt-frontend/window/points_view.h
@@ -10,15 +10,62 @@
 
 #include "ui_points_view.h"
 
+#include <any>
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#include <QPaintEvent>
 #include <QWidget>
+
+#include <stream_to_record/record_receive_i.h>
+#include <stream_to_record/stream_to_record_base.h>
 
 class points_view : public QWidget
 {
     Q_OBJECT
 
 private:
+    class record_receive_i : public user::record_receive_i
+    {
+    private:
+        points_view& parent;
+
+    public:
+        record_receive_i(points_view& parent) : parent(parent) {}
+
+    public:
+        void record_receive(const std::any& record) override
+        {
+            // 将 record 看作 32 位有符号整数。
+            // TODO: 支持自定义显示。
+            const auto& value = std::any_cast<const int32_t&>(record);
+            parent.points.push_back(value);
+            parent.repaint();
+        }
+    };
+    friend record_receive_i;
+
+private:
     Ui::points_view ui;
 
+private:
+    std::shared_ptr<record_receive_i> record_receive;
+    std::weak_ptr<user::stream_to_record_base> stream_to_record;
+
+private:
+    std::vector<int32_t> points;
+
 public:
-    points_view(QWidget* parent = nullptr);
+    std::shared_ptr<record_receive_i> get_record_receive()
+    {
+        return record_receive;
+    }
+
+public:
+    points_view(std::shared_ptr<user::stream_to_record_base> stream_to_record,
+                QWidget* parent = nullptr);
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
 };
